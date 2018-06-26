df23161038732113f94a2df4c8e73b982018e101
Alex Rodriguez <djale2008@gmail.com>
2018-06-26 12:39:14 -0400

    submited last notes

---
+++ Interview Questions/Untitled-5.rb
@@ -0,0 +1,98 @@
+June 23rd, 2018
+AWS Day 1 
+
+* Cloud Computing?
+    Application deployement, a broad and deep platform that helps custom,
+    the practice of using a network of remote servers hosted on the 
+    Internet to store, manage, and process data, rather than a local 
+    server or a personal computer.
+
+* Data lifecycle management policy(data retention period)
+    you keep files for a specif time and when audit happens they check 
+    your data retention files. 
+
+* route 53
+    Amazon VPC ec2 connection, 
+
+* BIG DATA?
+    cost efficent
+    greate manageability
+    Specialized services just for big data services
+    Scalable
+
+* Rehydration?
+    update of the Instance. 
+    delete the instance and recreate it again. 
+
+    4 environments takes = 1 takes 8 hours every 60 days. 
+
+    the purpose is to patch it up, Patch the OS basically fix bugs
+    also because its a security risk.
+
+* EC2 AUTO SCALING?
+
+* EC2 LOAD BALANCING?
+    doing a search each location will seach on local server.
+
+* Amazon Bucket?     S3 simple storage service 
+    buckets are gobally unique containers for everything you store 
+    in amazon S3.
+    - We user it to store .war file before we launche it. 
+    - After you create a bucket, you can upload your objects (for example, your photo or video files).
+    - By default, the permissions on an object are private, but you can set up access control policies 
+    to grant permissions to others.
+
+* Storage Gateway?
+    - 
+
+* RDS relational Database?
+    - 
+
+* DynamoDB - NOSQL Database?
+    - 
+
+* VPC Virtual Private Cloud?
+    - 
+
+* Route 53 - DNS Routing?
+    - 
+
+* Cloud Watch - Checking logs and monitor
+    - 
+
+* Lambda- serverless Application
+    - you write your code you executed only difference you dont need a server. 
+
+========== AWS MANAGEMENT CONSOLE =========
+
+We use AMIs when working at Capital One
+
+
+
+========== DAY 2 AWS  =========
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
